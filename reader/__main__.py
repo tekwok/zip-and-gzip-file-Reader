910c2adec4cb954c92107cc440aeb7fe7afb5afb
Donald Uviase <37631734+tekwok@users.noreply.github.com>
2018-10-09 10:21:23 +0100

    Update __main__.py

--- reader/__main__.py
+++ reader/__main__.py
@@ -1,7 +1,10 @@
+# import sys from the python repository
 import sys
 
+# import the reader module from the reader package
 import reader
 
+# read file in sys path with the reader class and close at the end
 r = reader.Reader(sys.argv[1])
 try:
     print(r.read())
